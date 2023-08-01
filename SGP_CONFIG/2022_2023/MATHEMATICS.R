ce00450a9a6626c55e397533ae45d80b7cd317a7
Damian Betebenner <dbetebenner@nciea.org>
2023-08-01 18:32:44 -0400

    Adding in 2022_2023 script

---
+++ SGP_CONFIG/2022_2023/MATHEMATICS.R
@@ -0,0 +1,11 @@
+#####################################################################################
+###                                                                               ###
+###    Consecutive-year SGP Configurations for 2023 MATHEMATICS subjects          ###
+###                                                                               ###
+#####################################################################################
+
+MATHEMATICS_2022_2023.config <- list(
+	MATHEMATICS_2022_2023 = list(
+		sgp.content.areas=rep("MATHEMATICS", 2),
+		sgp.panel.years=c("2021_2022", "2022_2023"),
+		sgp.grade.sequences=list(c("3", "4"), c("4", "5"), c("5", "6"), c("6", "7"), c("7", "8"))))
---
+++ Washington_SGP_2022_2023.R
@@ -0,0 +1,46 @@
+######################################################################################
+###                                                                                ###
+###                Washington SGP analyses for 2023                                ###
+###                                                                                ###
+######################################################################################
+
+###   Load packages
+require(SGP)
+require(SGPmatrices)
+
+###   Load data
+load("Data/Washington_Data_LONG.Rdata")
+
+###   Add Baseline matrices to SGPstateData
+SGPstateData <- addBaselineMatrices("WA", "2022_2023")
+
+###   Read in SGP Configuration Scripts and Combine
+source("SGP_CONFIG/2022_2023/READING.R")
+source("SGP_CONFIG/2022_2023/MATHEMATICS.R")
+
+WA_CONFIG <- c(READING_2022_2023.config, MATHEMATICS_2022_2023.config)
+
+### Parameters
+parallel.config <- NULL
+#parallel.config <- list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4))
+
+#####
+###   Run abcSGP analysis
+#####
+
+Washington_SGP <- updateSGP(
+        sgp_object = Washington_Data_LONG,
+        steps = c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
+        sgp.config = WA_CONFIG,
+        sgp.percentiles = TRUE,
+        sgp.projections = TRUE,
+        sgp.projections.lagged = TRUE,
+        sgp.percentiles.baseline = TRUE,
+        sgp.projections.baseline = TRUE,
+        sgp.projections.lagged.baseline = TRUE,
+        save.intermediate.results = FALSE,
+        parallel.config = parallel.config
+)
+
+###   Save results
+save(Washington_SGP, file="Data/Washington_SGP.Rdata")
